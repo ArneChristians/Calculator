6535bfc65dd1b84ead2b4b28b1583721f9714dad
ArneC <christiansarne04@gmail.com>
2025-09-19 12:01:18 +0200

    First thoughts of functions
    Just some tests to work on later

--- javascript.js
+++ javascript.js
@@ -5,10 +5,17 @@ function isNumber(text) {
     return /^[0-9]$/.test(text.trim());
 }
 
+function typing () {
+    if (isNumber(e.target.innerText)){
+            screen.textContent =`${screen.textContent}`+`${e.target.innerText}`;
+            console.log("Pressed: ", e.target.innerText);
+    }
+}
+
 cells.forEach(cell => {
     cell.addEventListener("click", e => {
         if (isNumber(e.target.innerText)){
-            screen.textContent =`${e.target.innerText}`;
+            screen.textContent =`${screen.textContent}`+`${e.target.innerText}`;
             console.log("Pressed: ", e.target.innerText);
         } else {
             console.log(e.target.innerText, " is not a Number!");
